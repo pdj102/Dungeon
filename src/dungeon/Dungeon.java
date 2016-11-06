437c921293c6f3dcce4824206dc4e751aeda3cb5
Paul Johnson <pdj102@yahoo.co.uk>
2016-11-06 18:30:59 +0000

    commit

--- src/dungeon/Dungeon.java
+++ src/dungeon/Dungeon.java
@@ -7,6 +7,7 @@ import java.awt.*;
 
 import javax.swing.JApplet;
 
+
 /**
  * @author Paul
  *
@@ -14,18 +15,57 @@ import javax.swing.JApplet;
 public class Dungeon extends JApplet{
 
 	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	/**
 	 * @param args
 	 */
+	
+	int mapSizeX = 40;
+	int mapSizeY = 40;
+	
+	int[][] map = new int[mapSizeX][mapSizeY];
+	
+	
 		
 	public void init() {
+		generateMap();
 	}
 	
-	public void paint(Graphics g) {
-	    g.drawString("Loveliest of trees, the cherry now", 25, 30);
-	    g.drawString("Is hung with bloom along the bough,", 25, 50);
-	    g.drawString("And stands about the woodland ride", 25, 70 );
-	    g.drawString("Wearing white for Eastertide." ,25, 90);
-	    g.drawString("--- A. E. Housman" ,50, 130);
+	public void generateMap() {
+		for (int i=0; i <10; i++) {
+			createRoom( (int)(Math.random() * (mapSizeX-10)), (int)(Math.random() * (mapSizeY-10)), (int)(Math.random() * 10), (int)(Math.random() * 10));
+		}
 	}
-
+	
+	public void createRoom(int xPos, int yPos, int sizeX, int sizeY) {
+		System.out.println(xPos);
+		System.out.println(yPos);
+		System.out.println(sizeX);
+		System.out.println(sizeY);
+		for (int x = xPos; x<xPos+sizeX; x++) {
+			for (int y = yPos; y<yPos+sizeY; y++) {
+				map[x][y] = 1;
+				System.out.print("*");
+			}
+		}
+	}
+	public void paint(Graphics g) {
+		g.setColor(Color.black);
+		for (int x = 0; x<mapSizeX; x++) {
+			for (int y = 0; y<mapSizeY; y++) {
+				switch (map[x][y]) {
+					case 1 : {
+						g.drawRect(x*20, y*20, 20, 20);
+						break;
+					}
+					default : { 
+						break;
+					}				
+				}
+				
+			}
+		}
+	}
 }
