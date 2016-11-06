c32c2dd8df985817569606a311fcedb077d9c742
Paul Johnson <pdj102@yahoo.co.uk>
2016-11-06 16:50:14 +0000

    commit

--- src/dungeon/Dungeon.java
+++ src/dungeon/Dungeon.java
@@ -3,20 +3,29 @@
  */
 package dungeon;
 
+import java.awt.*;
+
+import javax.swing.JApplet;
+
 /**
  * @author Paul
  *
  */
-public class Dungeon {
+public class Dungeon extends JApplet{
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		System.out.println("Hello World!");
-
+	public void init() {
+	}
+	
+	public void paint(Graphics g) {
+	    g.drawString("Loveliest of trees, the cherry now", 25, 30);
+	    g.drawString("Is hung with bloom along the bough,", 25, 50);
+	    g.drawString("And stands about the woodland ride", 25, 70 );
+	    g.drawString("Wearing white for Eastertide." ,25, 90);
+	    g.drawString("--- A. E. Housman" ,50, 130);
 	}
 
 }
